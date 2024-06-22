2db460cf5c9b4015d6102392371d2480204cff06
Juntao Qiu <juntao.qiu@gmail.com>
2024-06-22 13:17:10 +1000

    use filter with find to make the code a bit nicer

--- codemods/remove-feature-toggle/remove-feature-toggle.ts
+++ codemods/remove-feature-toggle/remove-feature-toggle.ts
@@ -2,64 +2,64 @@ import { Collection, JSCodeshift } from "jscodeshift";
 
 const removeFeatureToggle = (name: string) => {
   return (j: JSCodeshift, root: Collection) => {
-    root.find(j.IfStatement).forEach((isf) => {
-      const test = isf.node.test;
-
-      if (
-        test.type === "CallExpression" &&
-        test.callee.type === "Identifier" &&
-        test.callee.name === "featureToggle"
-      ) {
-        if (
-          test.arguments.length === 1 &&
-          test.arguments[0].type === "Literal" &&
-          test.arguments[0].value === name
-        ) {
-          if (isf.node.consequent.type === "BlockStatement") {
-            j(isf).replaceWith(isf.node.consequent.body);
-          }
+    root
+      .find(j.IfStatement, {
+        test: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "featureToggle",
+          },
+          arguments: [
+            {
+              type: "Literal",
+              value: name,
+            },
+          ],
+        },
+      })
+      .forEach((isf) => {
+        if (isf.node.consequent.type === "BlockStatement") {
+          j(isf).replaceWith(isf.node.consequent.body);
         }
-      }
-    });
+      });
 
-    root.find(j.ConditionalExpression).forEach(path => {
-      const test = path.node.test;
+    root
+      .find(j.ConditionalExpression, {
+        test: {
+          type: "CallExpression",
+          callee: { type: "Identifier", name: "featureToggle" },
+          arguments: [
+            {
+              type: (t: any) => t === "Literal" || t === "StringLiteral",
+              value: name,
+            },
+          ],
+        },
+      })
+      .forEach((path) => {
+        const consequent = path.node.consequent;
 
-      if (
-        test.type === "CallExpression" &&
-        test.callee.type === "Identifier" &&
-        test.callee.name === "featureToggle"
-      ) {
-        if (
-          test.arguments.length === 1 &&
-          (test.arguments[0].type === "Literal" ||
-            test.arguments[0].type === "StringLiteral") &&
-          test.arguments[0].value === name
-        ) {
-          const consequent = path.node.consequent;
-
-          // Handle VariableDeclarator
-          if (path.parent.node.type === "VariableDeclarator") {
-            path.parent.get("init").replace(consequent);
-          }
-
-          // Handle ReturnStatement
-          else if (path.parent.node.type === "ReturnStatement") {
-            path.parent.get("argument").replace(consequent);
-          }
-
-          // Handle AssignmentExpression
-          else if (path.parent.node.type === "AssignmentExpression") {
-            path.parent.get("right").replace(consequent);
-          }
-
-          // Handle other possible parent nodes as needed
-          else {
-            console.log(`Unhandled parent type: ${path.parent.node.type}`);
-          }
+        // Handle VariableDeclarator
+        if (path.parent.node.type === "VariableDeclarator") {
+          path.parent.get("init").replace(consequent);
         }
-      }
-    });
+
+        // Handle ReturnStatement
+        else if (path.parent.node.type === "ReturnStatement") {
+          path.parent.get("argument").replace(consequent);
+        }
+
+        // Handle AssignmentExpression
+        else if (path.parent.node.type === "AssignmentExpression") {
+          path.parent.get("right").replace(consequent);
+        }
+
+        // Handle other possible parent nodes as needed
+        else {
+          console.log(`Unhandled parent type: ${path.parent.node.type}`);
+        }
+      });
   };
 };
 
